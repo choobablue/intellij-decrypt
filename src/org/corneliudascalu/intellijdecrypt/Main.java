e06584b637d2719d676c5da3712670e807467426
Matt McHenry <mmchenry@metissecure.com>
2015-04-01 10:47:07 -0400

    fixed "odd number of characters"
    fixed "odd number of characters" error when an extra <options
    name="PASSWORDS"> element is included in security.xml

--- src/org/corneliudascalu/intellijdecrypt/Main.java
+++ src/org/corneliudascalu/intellijdecrypt/Main.java
@@ -9,57 +9,61 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 /**
  * @author Corneliu Dascalu <corneliu.dascalu@osf-global.com>
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        try {
-            File fXmlFile = null;
-            String password = null;
-            if (args.length == 0 || args[0].equalsIgnoreCase("-help")) {
-                System.out.println("-help Display this help message.\n-p <password> Specify the password\n-f " +
-                        "<path> (Optional) specify the path to the security.xml file. Default to ~/" +
-                        ".IdeaIC13/config/options/security.xml");
-                System.exit(0);
+        File fXmlFile = null;
+        String password = null;
+        if (args.length == 0 || args[0].equalsIgnoreCase("-help")) {
+            System.out.println("-help Display this help message.\n-p <password> Specify the password\n-f " +
+                    "<path> (Optional) specify the path to the security.xml file. Default to ~/" +
+                    ".IdeaIC13/config/options/security.xml");
+            System.exit(0);
+        }
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].equalsIgnoreCase("-f")) {
+                fXmlFile = new File(args[i + 1]);
+            } else if (args[i].equalsIgnoreCase("-p")) {
+                password = args[i + 1];
             }
-            for (int i = 0; i < args.length; i += 2) {
-                if (args[i].equalsIgnoreCase("-f")) {
-                    fXmlFile = new File(args[i + 1]);
-                } else if (args[i].equalsIgnoreCase("-p")) {
-                    password = args[i + 1];
-                }
-            }
-            if (password == null) {
-                System.out.println("Please specify the password");
-                System.exit(1);
-            }
-            if (fXmlFile == null) {
-                File homedir = new File(System.getProperty("user.home"));
-                fXmlFile = new File(homedir, ".IdeaIC13/config/options/security.xml");
-            }
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
+        }
+        if (password == null) {
+            System.out.println("Please specify the password");
+            System.exit(1);
+        }
+        if (fXmlFile == null) {
+            File homedir = new File(System.getProperty("user.home"));
+            fXmlFile = new File(homedir, ".IdeaIC13/config/options/security.xml");
+        }
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
 
-            NodeList passwords = doc.getElementsByTagName("option");
-            if (passwords.getLength() > 1) {
-                System.out.println("XML file OK");
-                for (int i = 1; i < passwords.getLength(); i++) {
-                    System.out.println(passwords.item(i).getAttributes().item(0).getNodeValue());
+        NodeList passwords = doc.getElementsByTagName("option");
+        if (passwords.getLength() > 1) {
+            System.out.println("XML file OK");
+            for (int i = 1; i < passwords.getLength(); i++) {
+                NamedNodeMap attrs = passwords.item(i).getAttributes();
+                Node attrValue = attrs.getNamedItem("value");
+                if (attrValue != null) {
+                    System.out.println(attrValue.getNodeValue());
                     try {
                         System.out.println(EncryptionUtil.decryptText(EncryptionUtil.genPasswordKey(password),
-                                fromHex(passwords.item(i).getAttributes().item(0).getNodeValue())));
+                                fromHex(attrValue.getNodeValue())));
                     } catch (IllegalStateException e) {
                         System.out.println("Not a password");
+                    } catch (DecoderException e) {
+                        System.out.println("Error decoding");
                     }
                 }
-            } else {
-                System.out.println("Unexpected xml file structure or no passwords stored");
             }
-        } catch (DecoderException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("Unexpected xml file structure or no passwords stored");
         }
     }
 
